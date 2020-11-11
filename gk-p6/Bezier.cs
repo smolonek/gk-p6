e54d1bbf9c63bd332f65a328d33a8bc52a9eb4ec
Adam <smolonek@gmail.com>
2020-11-11 14:09:27 +0100

    wszystko, tylko to ostatnie troche zbugowane

--- gk-p6/Bezier.cs
+++ gk-p6/Bezier.cs
@@ -16,45 +16,12 @@ namespace gk_p6
 		private const float dt = 0.01f;
 		private static long BinomialCoefficient(long n, long k)
 		{
-			//if (k > n - k) k = n - k;
-			//long outcome = 1;
-			//if (n == k || k == 0)
-			//	return 1;
-			//for(int i = 0; i <= k; ++i)
-			//{
-			//	outcome *= n - (k - i);
-			//	outcome /= i;
-			//}
-			//return outcome;
 			if ((n == k) || (k == 0))
 				return 1;
 			else
 				return BinomialCoefficient(n - 1, k) + BinomialCoefficient(n - 1, k - 1);
 		}
-		private static double GetX(float t, Point[] points)
-		{
-			double sum = 0;
-			long n = points.Length - 1;
-			for(long i = 0; i < n; i++)
-			{
-				sum += points[i].X * BinomialCoefficient(n, i) * Math.Round(Math.Pow(t, i), 2) * Math.Round(Math.Pow((1 - t), (n - i)), 2);
-			}
-			return Math.Round(sum, 2);
-		}
-		private static double GetY(float t, Point[] points)
-		{
-			double sum = 0;
-			long n = points.Length - 1;
-			Trace.WriteLine("-----------START-----------");
-			for (long i = 0; i < n; i++)
-			{
-				sum += points[i].Y * BinomialCoefficient(n, i) * Math.Round(Math.Pow(t, i), 2) * Math.Round(Math.Pow((1 - t), (n - i)), 2);
-				Trace.WriteLine(string.Format("{0} * {1} * {2} * {3}", points[i].Y, BinomialCoefficient(n, i), Math.Round(Math.Pow(t, i), 2), Math.Round(Math.Pow((1 - t), (n - i)), 2)));
-			}
-			Trace.WriteLine("-----------END-----------");
-			return Math.Round(sum, 2);
-		}
-		private static double X(double t, List<Point> points)
+		private static double GetX(double t, List<Point> points)
 		{
 			//todo: sprobowac foreachami, elementat(item) albo w ogole casteljau czy jak mu tam
 			double sum = 0.0;
@@ -66,7 +33,7 @@ namespace gk_p6
 			}
 			return sum;
 		}
-		private static double Y(double t, List<Point> points)
+		private static double GetY(double t, List<Point> points)
 		{
 			double sum = 0.0;
 			int n = points.Count - 1;
@@ -80,29 +47,11 @@ namespace gk_p6
 		public static List<Point> GetPoints(List<Point> controlPoints)
 		{
 			List<Point> points = new List<Point>();
-			Point[] ctrl = controlPoints.ToArray();
 			for (float t = 0.0f; t < 1.0; t += dt)
 			{
-				points.Add(new Point(GetX(t, ctrl), GetY(t, ctrl)));
+				points.Add(new Point(GetX(t, controlPoints), GetY(t, controlPoints)));
 			}
-			points.Add(new Point(GetX(1.0f, ctrl), GetY(1.0f, ctrl)));
-			return points;
-		}
-		public static List<Point> DrawBezier(List<Point> controlPoints)
-		{
-			// Draw the curve.
-			List<Point> points = new List<Point>();
-			for (float t = 0.0f; t < 1.0; t += dt)
-			{
-				points.Add(new Point(
-					X(t, controlPoints),
-					Y(t, controlPoints)));
-			}
-
-			// Connect to the final point.
-			points.Add(new Point(
-				X(1.0f, controlPoints),
-				Y(1.0f, controlPoints)));
+			points.Add(new Point(GetX(1.0f, controlPoints),	GetY(1.0f, controlPoints)));
 			return points;
 		}
 	}
--- gk-p6/MainWindow.xaml.cs
+++ gk-p6/MainWindow.xaml.cs
@@ -13,141 +13,248 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 using Color = System.Windows.Media.Color;
 using Point = System.Windows.Point;
 using gk_p6;
 
 namespace gk_p6
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-			List<Point> points = new List<Point>();
-			Random rng = new Random();
-			//for(int i = 0; i < 25; i++)
-			//         {
-			//	//points.Add(new Point(rng.Next(450), rng.Next(800)));
-			//	points.Add(new Point(i, i));
-			//         }
-			points.Add(new Point(100, 100));
-            points.Add(new Point(300, 100));
-            points.Add(new Point(300, 300));
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Point> controlPoints = new List<Point>();
+        List<Ellipse> ellipses = new List<Ellipse>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //List<Point> points = new List<Point>();
+            //points.Add(new Point(100, 100));
+            //points.Add(new Point(300, 100));
+            //points.Add(new Point(300, 300));
             //points.Add(new Point(200, 150));
-            DrawPoints(points);
-		}
-		private void DrawPoints(List<Point> points)
-		{
+            //DrawPoints(points);
+        }
+        private void DrawPoints(List<Point> points)
+        {
             Path path = MakeCurve(Bezier.GetPoints(points).ToArray(), 0.01);
             path.Stroke = System.Windows.Media.Brushes.Black;
             path.StrokeThickness = 2;
-            Canvas.SetLeft(path, points.ElementAt(0).X);
-            Canvas.SetTop(path, points.ElementAt(0).Y);
             Canvas.Children.Add(path);
-            List<Point> bezier = Bezier.DrawBezier(points);
-            foreach (var item in bezier)
-            {
-                Ellipse ellipse = new Ellipse();
-                ellipse.Fill = System.Windows.Media.Brushes.Black;
-                ellipse.StrokeThickness = 2;
-                ellipse.Stroke = System.Windows.Media.Brushes.Black;
-                ellipse.Width = 10;
-                ellipse.Height = 10;
-                Canvas.SetLeft(ellipse, item.X);
-                Canvas.SetTop(ellipse, item.Y);
-                Canvas.Children.Add(ellipse);
-            }
+            //List<Point> bezier = Bezier.GetPoints(points);
+            //foreach (var item in bezier)
+            //{
+            //    Ellipse ellipse = new Ellipse();
+            //    ellipse.Fill = System.Windows.Media.Brushes.Black;
+            //    ellipse.StrokeThickness = 2;
+            //    ellipse.Stroke = System.Windows.Media.Brushes.Black;
+            //    ellipse.Width = 10;
+            //    ellipse.Height = 10;
+            //    Canvas.SetLeft(ellipse, item.X);
+            //    Canvas.SetTop(ellipse, item.Y);
+            //    Canvas.Children.Add(ellipse);
+            //}
+            //ClearControlPoints();
             foreach (var point in points)
             {
                 Ellipse ellipse = new Ellipse();
-                SolidColorBrush color = new SolidColorBrush();
-                color.Color = Color.FromArgb(255, 255, 255, 0);
-                ellipse.Fill = color;
+                ellipse.Fill = System.Windows.Media.Brushes.LightGray;
                 ellipse.StrokeThickness = 2;
-                ellipse.Stroke = System.Windows.Media.Brushes.Red;
+                ellipse.Stroke = System.Windows.Media.Brushes.Gray;
                 ellipse.Width = 16;
                 ellipse.Height = 16;
-                Canvas.SetLeft(ellipse, point.X);
-                Canvas.SetTop(ellipse, point.Y);
+                Canvas.SetLeft(ellipse, point.X - ellipse.Width / 2);
+                Canvas.SetTop(ellipse, point.Y - ellipse.Width / 2);
                 Canvas.Children.Add(ellipse);
             }
         }
         private Path MakeBezierPath(Point[] points)
-		{
-			Path path = new Path();
-			PathGeometry pathGeometry = new PathGeometry();
-			path.Data = pathGeometry;
-			PathFigure pathFigure = new PathFigure();
-			pathGeometry.Figures.Add(pathFigure);
-			pathFigure.StartPoint = points[0];
-			PathSegmentCollection pathSegments = new PathSegmentCollection();
-			pathFigure.Segments = pathSegments;
-			PointCollection pointsCollection = new PointCollection(points.Length - 1);
-			for(int i = 1; i < points.Length; i++)
+        {
+            Path path = new Path();
+            PathGeometry pathGeometry = new PathGeometry();
+            path.Data = pathGeometry;
+            PathFigure pathFigure = new PathFigure();
+            pathGeometry.Figures.Add(pathFigure);
+            pathFigure.StartPoint = points[0];
+            PathSegmentCollection pathSegments = new PathSegmentCollection();
+            pathFigure.Segments = pathSegments;
+            PointCollection pointsCollection = new PointCollection(points.Length - 1);
+            for (int i = 1; i < points.Length; i++)
             {
-				pointsCollection.Add(points[i]);
+                pointsCollection.Add(points[i]);
             }
-			PolyBezierSegment polyBezierSegment = new PolyBezierSegment();
-			polyBezierSegment.Points = pointsCollection;
-			pathSegments.Add(polyBezierSegment);
-			return path;
+            PolyBezierSegment polyBezierSegment = new PolyBezierSegment();
+            polyBezierSegment.Points = pointsCollection;
+            pathSegments.Add(polyBezierSegment);
+            return path;
         }
-		private Point[] MakeCurvePoints(Point[] points, double tension)
-		{
-			if (points.Length < 2) return null;
-			double control_scale = tension / 0.5 * 0.175;
+        private Point[] MakeCurvePoints(Point[] points, double tension)
+        {
+            if (points.Length < 2) return null;
+            double control_scale = tension / 0.5 * 0.175;
 
-			// Make a list containing the points and
-			// appropriate control points.
-			List<Point> result_points = new List<Point>();
-			result_points.Add(points[0]);
+            // Make a list containing the points and
+            // appropriate control points.
+            List<Point> result_points = new List<Point>();
+            result_points.Add(points[0]);
 
-			for (int i = 0; i < points.Length - 1; i++)
-			{
-				// Get the point and its neighbors.
-				Point pt_before = points[Math.Max(i - 1, 0)];
-				Point pt = points[i];
-				Point pt_after = points[i + 1];
-				Point pt_after2 = points[Math.Min(i + 2, points.Length - 1)];
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                // Get the point and its neighbors.
+                Point pt_before = points[Math.Max(i - 1, 0)];
+                Point pt = points[i];
+                Point pt_after = points[i + 1];
+                Point pt_after2 = points[Math.Min(i + 2, points.Length - 1)];
 
-				double dx1 = pt_after.X - pt_before.X;
-				double dy1 = pt_after.Y - pt_before.Y;
+                double dx1 = pt_after.X - pt_before.X;
+                double dy1 = pt_after.Y - pt_before.Y;
 
-				Point p1 = points[i];
-				Point p4 = pt_after;
+                Point p1 = points[i];
+                Point p4 = pt_after;
 
-				double dx = pt_after.X - pt_before.X;
-				double dy = pt_after.Y - pt_before.Y;
-				Point p2 = new Point(
-					pt.X + control_scale * dx,
-					pt.Y + control_scale * dy);
+                double dx = pt_after.X - pt_before.X;
+                double dy = pt_after.Y - pt_before.Y;
+                Point p2 = new Point(
+                    pt.X + control_scale * dx,
+                    pt.Y + control_scale * dy);
 
-				dx = pt_after2.X - pt.X;
-				dy = pt_after2.Y - pt.Y;
-				Point p3 = new Point(
-					pt_after.X - control_scale * dx,
-					pt_after.Y - control_scale * dy);
+                dx = pt_after2.X - pt.X;
+                dy = pt_after2.Y - pt.Y;
+                Point p3 = new Point(
+                    pt_after.X - control_scale * dx,
+                    pt_after.Y - control_scale * dy);
 
-				// Save points p2, p3, and p4.
-				result_points.Add(p2);
-				result_points.Add(p3);
-				result_points.Add(p4);
-			}
+                // Save points p2, p3, and p4.
+                result_points.Add(p2);
+                result_points.Add(p3);
+                result_points.Add(p4);
+            }
 
-			// Return the points.
-			return result_points.ToArray();
-		}
-		private Path MakeCurve(Point[] points, double tension)
-		{
-			if (points.Length < 2) return null;
-			Point[] result_points = MakeCurvePoints(points, tension);
+            // Return the points.
+            return result_points.ToArray();
+        }
+        private Path MakeCurve(Point[] points, double tension)
+        {
+            if (points.Length < 2) return null;
+            Point[] result_points = MakeCurvePoints(points, tension);
 
-			// Use the points to create the path.
-			return MakeBezierPath(result_points.ToArray());
-		}
-	}
+            // Use the points to create the path.
+            return MakeBezierPath(result_points.ToArray());
+        }
+
+        private void ClearCanvas_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.Children.Clear();
+            controlPoints.Clear();
+            ellipses.Clear();
+        }
+
+
+        //      private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        //      {
+        //	Ellipse point = new Ellipse();
+        //	point.Fill = System.Windows.Media.Brushes.LightGray;
+        //	point.StrokeThickness = 2;
+        //	point.Stroke = System.Windows.Media.Brushes.Gray;
+        //	point.Width = 16;
+        //	point.Height = 16;
+        //	Canvas.SetLeft(point, e.GetPosition(this).X - point.Width / 2);
+        //	Canvas.SetTop(point, e.GetPosition(this).Y - point.Width / 2);
+        //	Canvas.Children.Add(point);
+        //}
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse point = new Ellipse();
+            point.Fill = System.Windows.Media.Brushes.LightGray;
+            point.StrokeThickness = 2;
+            point.Stroke = System.Windows.Media.Brushes.Gray;
+            point.Width = 16;
+            point.Height = 16;
+            point.PreviewMouseMove += ControlPoint_MouseMove;
+            double x = e.GetPosition(this).X, y = e.GetPosition(this).Y;
+            Canvas.SetLeft(point, x - point.Width / 2);
+            Canvas.SetTop(point, y - point.Width / 2);
+            Canvas.Children.Add(point);
+            Canvas.SetZIndex(point, 9999);
+            controlPoints.Add(new Point(x, y));
+            ellipses.Add(point);
+        }
+
+        private void DrawBezier_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.Children.Clear();
+            DrawPoints(controlPoints);
+            for (int i = Canvas.Children.Count - 1; i >= 0; i--)
+            {
+                if (Canvas.Children[i].GetType() == typeof(Ellipse))
+                    Canvas.Children.Remove(Canvas.Children[i]);
+            }
+            ellipses.Clear();
+            foreach (var item in controlPoints)
+            {
+                Ellipse point = new Ellipse();
+                point.Fill = System.Windows.Media.Brushes.LightGray;
+                point.StrokeThickness = 2;
+                point.Stroke = System.Windows.Media.Brushes.Gray;
+                point.Width = 16;
+                point.Height = 16;
+                point.MouseMove += ControlPoint_MouseMove;
+                double x = item.X, y = item.Y;
+                Canvas.SetLeft(point, x - point.Width / 2);
+                Canvas.SetTop(point, y - point.Width / 2);
+                Canvas.Children.Add(point);
+                ellipses.Add(point);
+            }
+        }
+        private void ControlPoint_MouseMove(object sender, MouseEventArgs e)
+        {
+            var ellipse = (Ellipse)sender;
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                CordsLabel.Content = string.Format("X: {0}, Y: {1}", e.GetPosition(this).X, e.GetPosition(this).Y);
+                //MessageBox.Show("PO");
+                int index = 0;
+                index = ellipses.IndexOf(ellipse);
+                Canvas.SetLeft(ellipse, e.GetPosition(this).X - ellipse.ActualWidth / 2);
+                Canvas.SetTop(ellipse, e.GetPosition(this).Y - ellipse.ActualHeight / 2);
+                Point point = new Point();
+                point.X = e.GetPosition(this).X;
+                point.Y = e.GetPosition(this).Y;
+                controlPoints[index] = point;
+                DeletePath();
+                //ClearControlPoints();
+                //Canvas.Children.Clear();
+                DrawPoints(controlPoints);
+            }
+        }
+        private void DeletePath()
+        {
+            for (int i = Canvas.Children.Count - 1; i >= 0; i--)
+            {
+                if(Canvas.Children[i].GetType() == typeof(Path))
+                    Canvas.Children.Remove(Canvas.Children[i]);
+            }
+        }
+        private void ClearControlPoints()
+        {
+            List<Ellipse> tmp = new List<Ellipse>();
+            for (int i = Canvas.Children.Count - 1; i >= 0; i--)
+            {
+                Trace.WriteLine(Canvas.Children[i].GetType().ToString() + " : " + Canvas.Children.Count);
+                if (Canvas.Children[i].GetType() == typeof(Ellipse))
+                    tmp.Add((Ellipse)Canvas.Children[i]);
+            }
+            foreach(var item in tmp)
+            {
+                Canvas.Children.Remove(item);
+            }
+        }
+        private void ClearControlPoints_Click(object sender, RoutedEventArgs e)
+        {
+            ClearControlPoints();
+        }
+    }
 }
